bcd0d6c57d9b1127827680022164bf2156ec2f42
jasontonk <jj.tonk@student.avans.nl>
2020-04-17 10:41:42 +0200

    deleted files

---
+++ GitPractice2/src/com/avans/hellogit/GitPractice.java
@@ -0,0 +1,8 @@
+package com.avans.hellogit;
+
+public class GitPractice {
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub");
+		System.out.println("Sorry, we zijn nog niet bij GitHub aangekomen.");
+	}
+}
