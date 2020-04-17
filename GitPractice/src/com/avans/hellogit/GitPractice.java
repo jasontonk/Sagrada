f67fa794e651fdbbe66962e951640e5e243fbf59
jasontonk <jj.tonk@student.avans.nl>
2020-04-17 11:06:25 +0200

    extra shit

--- GitPractice/src/com/avans/hellogit/GitPractice.java
+++
@@ -1,8 +0,0 @@
-package com.avans.hellogit;
-
-public class GitPractice {
-	public static void main(String[] args) {
-		System.out.println("Hello GitHub");
-		System.out.println("Sorry, we zijn nog niet bij GitHub aangekomen.");
-	}
-}
