9eb408cb32f7b37aff40d21fbe9fdf37b6e03f72
spookyblaz <siemvoirschot@gmail.com>
2020-04-19 14:18:34 +0200

    place dice method

--- Sagrada/src/model/RoundTrackField.java
+++ Sagrada/src/model/RoundTrackField.java
@@ -1,5 +1,23 @@
 package model;
 
 public class RoundTrackField {
+	
+	Dice[][] diceSavingSpots = new Dice[10][9]
+			
+			public RoundTrackField() {
+				// TODO Auto-generated constructor stub
+		
+			}
+			
+	public void placeDice(Dice dice, round int) {
+		for (int i = 0; i < diceSavingSpots.length; i++) {
+			if (diceSavingSpots[round][i] == null) {
+				diceSavingSpots[round][i] = dice;	
+				return;
+			
+			}
+		}
+				
+	}
 
 }
