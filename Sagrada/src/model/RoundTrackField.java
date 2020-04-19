54407b9bf1cc64940fbb500285ac51af7b1c0b75
spookyblaz <siemvoirschot@gmail.com>
2020-04-19 14:32:03 +0200

    added remove dice method

--- Sagrada/src/model/RoundTrackField.java
+++ Sagrada/src/model/RoundTrackField.java
@@ -1,23 +1,26 @@
 package model;
 
 public class RoundTrackField {
-	
+
 	Dice[][] diceSavingSpots = new Dice[10][9]
-			
-			public RoundTrackField() {
-				// TODO Auto-generated constructor stub
-		
-			}
-			
-	public void placeDice(Dice dice, round int) {
+
+	public void placeDice(Dice dice, round int) { // methode je geeft een dobbelsteen mee en de ronde en plaatst de dobbelsteen op het eerste lege plekje in het ronde spoor
 		for (int i = 0; i < diceSavingSpots.length; i++) {
 			if (diceSavingSpots[round][i] == null) {
 				diceSavingSpots[round][i] = dice;	
 				return;
-			
 			}
-		}
-				
+		}				
 	}
+
+	public Dice takeDice(round int, dice int) {
+		if (diceSavingSpots[round][dice] != null) {
+			Dice returndice = diceSavingSpots[round][dice];
+			diceSavingSpots[round][dice] = null;
+			return returndice;
+		}
+		 return dice;
+	}
+	
 
 }
