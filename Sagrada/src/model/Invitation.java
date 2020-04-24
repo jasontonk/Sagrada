4cf2ba9e78699ac873aab3af1f156037e91a9c94
rutgerw1 <rutger.willems@hotmail.com>
2020-04-24 16:22:28 +0200

    Invitation toegevoegd

---
+++ Sagrada/src/model/Invitation.java
@@ -0,0 +1,31 @@
+
+public class Invitation {
+	
+	private String message;
+	private Player sender;
+	private Player receiver;
+	
+	
+	//Constructor that adds sender, receiver and message to the invitation 
+	public Invitation(Player sendingPlayer, Player receivingPlayer, String message) {
+		addSender(sendingPlayer);
+		addReceiver(receivingPlayer);
+		addText(message);
+	}
+	
+	//adds the player who sent the invitation
+	private void addSender(Player sendingPlayer) {
+		sender = sendingPlayer;
+	}
+	
+	//adds the player who should receiver the invitation
+	private void addReceiver(Player receivingPlayer) {
+		receiver = receivingPlayer;
+	}
+	
+	//adds the message that should be sent with the invitation
+	private void addText(String text) {
+		message = text;
+	}
+	
+}
