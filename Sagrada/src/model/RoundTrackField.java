ec4a21afd0f509455ad9522cea13fd12619a8ac4
spookyblaz <siemvoirschot@gmail.com>
2020-04-24 13:32:15 +0200

    comentaar toegevoegd

--- Sagrada/src/model/RoundTrackField.java
+++ Sagrada/src/model/RoundTrackField.java
@@ -2,23 +2,23 @@ package model;
 
 public class RoundTrackField {
 
-	private Die[] dieSavingSpot;
+	private Die[] dieSavingSpot;// array to save dies max 9 dies per round
 	
 	public RoundTrackField() {
 		// TODO Auto-generated constructor stub
 		dieSavingSpot = new Die[9];
 	}
 
-	public void addDice(Die die) {
+	public void addDice(Die die) { // adds die to the dieSavingSpot array on the first possible place
 		for (int i = 0; i < dieSavingSpot.length; i++) {
 			if (dieSavingSpot[i] == null) {
 				dieSavingSpot[i] = i;
-				return;
+				break;
 			}	
 		}
 	}
 	
-	public die takeDie(int die) {
+	public die takeDie(int die) { // select number of die in array returns die that was selected if die not null and removes it from the array
 		Die returndie;
 		if (die != null) {
 			returndie = dieSavingSpot[die];
@@ -31,6 +31,7 @@ public class RoundTrackField {
 	public Die[] getDieSavingSpot() {
 		return dieSavingSpot;
 	}
+	
 	public void setDieSavingSpot(Die[] dieSavingSpot) {
 		this.dieSavingSpot = dieSavingSpot;
 	}
