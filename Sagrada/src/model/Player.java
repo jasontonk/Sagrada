46512c536506070a2b0da0413fd8cb4de725ce3e
Milanvanoeveren <m.vanoeveren1@student.avans.nl>
2020-04-27 10:23:34 +0200

    1.3

--- Sagrada/src/model/Player.java
+++ Sagrada/src/model/Player.java
@@ -2,9 +2,11 @@ package model;
 
 public class Player {
 	
+	private Account account;
 	private String name;
 	private PatternCard patternCard;
 	private FavorToken favorToken;
 	private Color color;
-	
+	private int score;
+	private Game game;
 }
