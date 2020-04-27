58392d7f0cc6914d406e9aebfa5e9f2cbcc48639
jasontonk <jj.tonk@student.avans.nl>
2020-04-27 11:48:35 +0200

    Added getters

--- Sagrada/src/model/PublicObjectiveCard.java
+++ Sagrada/src/model/PublicObjectiveCard.java
@@ -8,4 +8,10 @@ public class PublicObjectiveCard {
 		this.name = name;
 		this.description = description;
 	}
+	public String getName() {
+		return name;
+	}
+	public String getDescription() {
+		return description;
+	}
 }
