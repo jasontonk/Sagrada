a02c1929f03f9fa6d2f28e3b565d01d9368a70b5
spookyblaz <siemvoirschot@gmail.com>
2020-04-27 11:19:52 +0200

    added instance variables

--- Sagrada/src/model/Game.java
+++ Sagrada/src/model/Game.java
@@ -5,4 +5,15 @@ public class Game {
 	//add toolcard
 	//add public objective
 //add gamedie	//add chat ?
+	private Player[] Players
+	private Board Board
+	private Toolcards[] toolcards
+	private Objectivecard[] objectiveCards
+	private Gamedie[] diesinBag
+	private RoundTrack roundTrack
+	private Chat chat
+	
+	public void play() {
+		
+	}
 }
