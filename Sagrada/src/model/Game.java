6a9690ecd1a92ba85482cb3a5012d01fcbe927b0
Azmat <t.aizimaiti@student.avans.nl>
2020-04-29 12:19:21 +0200

    Game constructor aangemaakt voor 2,3,4 players.

--- Sagrada/src/model/Game.java
+++ Sagrada/src/model/Game.java
@@ -5,10 +5,11 @@ import java.util.ArrayList;
 public class Game {
 // add players
 // add roundtrack
-	//add toolcard
-	//add public objective
+	// add toolcard
+	// add public objective
 //add gamedie	//add chat ?
 	private ArrayList<Player> players;
+	private ArrayList<Account> accounts;
 	private Board board;
 	private ArrayList<Toolcard> toolcards;
 	private ArrayList<PublicObjectiveCard> publicObjectiveCards;
@@ -16,9 +17,66 @@ public class Game {
 	private RoundTrack roundTrack;
 	private Chat chat;
 	private int round;
-	
+
+	public Game(Account account1, Account account2) {
+		players = new ArrayList<Player>();
+		toolcards = new ArrayList<Toolcard>();
+		publicObjectiveCards = new ArrayList<PublicObjectiveCard>();
+		diesInBag = new GameDie[90];
+		round = 1;
+
+		accounts.add(account1);
+		accounts.add(account2);
+
+		checkInvites();
+
+	}
+
+	public Game(Account account1, Account account2, Account account3) {
+		players = new ArrayList<Player>();
+		toolcards = new ArrayList<Toolcard>();
+		publicObjectiveCards = new ArrayList<PublicObjectiveCard>();
+		diesInBag = new GameDie[90];
+		round = 1;
+
+		accounts.add(account1);
+		accounts.add(account2);
+		accounts.add(account3);
+
+		checkInvites();
+
+	}
+
+	public Game(Account account1, Account account2, Account account3, Account account4) {
+		players = new ArrayList<Player>();
+		toolcards = new ArrayList<Toolcard>();
+		publicObjectiveCards = new ArrayList<PublicObjectiveCard>();
+		diesInBag = new GameDie[90];
+		round = 1;
+
+		accounts.add(account1);
+		accounts.add(account2);
+		accounts.add(account3);
+		accounts.add(account4);
+
+		checkInvites();
+
+	}
+
+	public void checkInvites() {
+		for (Account account : accounts) {
+
+			if (account.getInvitations() && account.accepInvitations) {
+				Player player1 = new Player();
+				account.setPlayers(player1);
+				account.setAccountStatus(accountStatus);
+				players.add(account.getPlayer());
+			}
+		}
+	}
+
 	public void play() {
-		
+
 	}
 
 	public ArrayList<Player> getPlayers() {
