219497e2d0c5713c188a72d17b35d1907b9b396e
Azmat <t.aizimaiti@student.avans.nl>
2020-04-30 17:36:23 +0200

    chat en favortoken klassen gekoppeld aan database.

--- Sagrada/src/model/Chat.java
+++ Sagrada/src/model/Chat.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on branch 'master' of https://github.com/jasontonk/Sagrada
 package model;
 
 import java.sql.Timestamp;
@@ -57,3 +58,71 @@ public class Chat {
 		this.message = message;
 	}
 }
+=======
+package model;
+
+import java.sql.Timestamp;
+
+import database.ChatDBA;
+import database.DataBaseConnection;
+
+public class Chat {
+	private Timestamp time;
+	private int playerId;
+	private String message;
+	private ChatDBA chatDB;
+
+	// Partial constructor, generate the player id and message of the player.
+
+	public Chat(int playerid, String message, DataBaseConnection c) {
+
+		playerId = playerid;
+
+		this.message = message;
+
+		chatDB = new ChatDBA(c);
+
+	}
+
+	// Return the time stamp of this chat.
+
+	public Timestamp getTime() {
+
+		return time;
+	}
+
+	// Set the time stamp of this chat.
+	public void setTime(Timestamp time) {
+
+		this.time = time;
+	}
+
+	// Return the player id of this chat.
+
+	public int getPlayerId() {
+
+		return playerId;
+	}
+
+	// Set the player id of this chat.
+
+	public void setPlayerId(int playerId) {
+
+		this.playerId = playerId;
+	}
+
+	// Return the message of this chat.
+
+	public String getMessage(int playerid, Timestamp timestamp) {
+
+		return chatDB.getChatTextDB(playerid, timestamp);
+	}
+
+	// Set the message of this chat.
+
+	public void setMessage(int playerid, String message, Chat chatline) {
+		chatDB.addChatDB(playerid, message, this);
+	}
+
+}
+>>>>>>> 9bc128a chat en favortoken klassen gekoppeld aan database.
--- Sagrada/src/model/FavorToken.java
+++ Sagrada/src/model/FavorToken.java
@@ -1,3 +1,4 @@
+<<<<<<< Upstream, based on branch 'master' of https://github.com/jasontonk/Sagrada
 package model;
 
 public class FavorToken {
@@ -81,3 +82,80 @@ public class FavorToken {
 	}
 
 }
+=======
+package model;
+
+import database.DataBaseConnection;
+import database.FavorTokenDBA;
+
+public class FavorToken {
+
+	private int id;
+	private int gameId;
+	private Player player;
+	private Toolcard toolcard;
+	private FavorTokenDBA db;
+
+	// Partial constructor, generate favor tokens of player.
+
+	public FavorToken(int id, Player player, int gameid, DataBaseConnection c) {
+
+		this.id = id;
+
+		this.player = player;
+
+		gameId = gameid;
+
+		db = new FavorTokenDBA(c);
+
+	}
+
+	// Return the id of FavorToken.
+
+	public int getId() {
+		return 1;
+	}
+
+	// Return the game of player.
+
+	public int getGameId() {
+
+		return gameId;
+	}
+
+	// Set the game of is FavorToken is located.
+
+	public void setGameId(int gameid) {
+		db.addFavorToken(gameid);
+	}
+
+	// Return the player of this FavorToken.
+
+	public Player getPlayer() {
+
+		return player;
+	}
+
+	// Set the player of this FavorToken.
+
+	public void setPlayer(Player player) {
+
+		this.player = player;
+	}
+
+	// Return the Toolcard of which this FavorToken is used for.
+
+	public Toolcard getToolcard() {
+
+		return toolcard;
+	}
+
+	// Set the Toolcard of which this FavorToken is used for.
+
+	public void setToolcard(int playerid, int toolcard, int round, int favortokenid) {
+		db.setFavortokensForToolCard(playerid, toolcard, round, favortokenid);
+
+	}
+
+}
+>>>>>>> 9bc128a chat en favortoken klassen gekoppeld aan database.
