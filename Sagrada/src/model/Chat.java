0e9fceeb2e47521d754d3bd6656e563b229678d8
Azmat <t.aizimaiti@student.avans.nl>
2020-04-30 17:39:23 +0200

    beide klassen gekoppeld aan database.

--- Sagrada/src/model/Chat.java
+++ Sagrada/src/model/Chat.java
@@ -1,64 +1,4 @@
-<<<<<<< Upstream, based on branch 'master' of https://github.com/jasontonk/Sagrada
-package model;
 
-import java.sql.Timestamp;
-
-public class Chat {
-	private Timestamp time;
-	private int playerId;
-	private String message;
-
-	// Partial constructor, generate the player id and message of the player.
-
-	public Chat(int playerid, String message) {
-
-		playerId = playerid;
-
-		this.message = message;
-	}
-
-	// Return the time stamp of this chat.
-
-	public Timestamp getTime() {
-
-		return time;
-	}
-
-	// Set the time stamp of this chat.
-	public void setTime(Timestamp time) {
-
-		this.time = time;
-	}
-
-	// Return the player id of this chat.
-
-	public int getPlayerId() {
-
-		return playerId;
-	}
-
-	// Set the player id of this chat.
-
-	public void setPlayerId(int playerId) {
-
-		this.playerId = playerId;
-	}
-
-	// Return the message of this chat.
-
-	public String getMessage() {
-
-		return message;
-	}
-
-	// Set the message of this chat.
-
-	public void setMessage(String message) {
-
-		this.message = message;
-	}
-}
-=======
 package model;
 
 import java.sql.Timestamp;
@@ -125,4 +65,3 @@ public class Chat {
 	}
 
 }
->>>>>>> 9bc128a chat en favortoken klassen gekoppeld aan database.
--- Sagrada/src/model/FavorToken.java
+++ Sagrada/src/model/FavorToken.java
@@ -1,88 +1,4 @@
-<<<<<<< Upstream, based on branch 'master' of https://github.com/jasontonk/Sagrada
-package model;
 
-public class FavorToken {
-
-	private int id;
-	private Game game;
-	private Player player;
-	private Toolcard toolcard;
-
-	// Partial constructor, generate favor tokens of player.
-
-	public FavorToken(int id, Player player) {
-
-		this.id = id;
-
-		this.player = player;
-
-		this.game = player.getGame();
-		
-
-	}
-	
-	public FavorToken(int id) {
-		
-	}
-
-	// Return the id of FavorToken.
-
-	public int getId() {
-
-		return id;
-	}
-
-	// Set the id of FavorToken.
-
-	public void setId(int id) {
-
-		this.id = id;
-	}
-
-	// Return the game of player.
-
-	public Game getGame() {
-
-		return game;
-	}
-
-	// Set the game of is FavorToken is located.
-
-	public void setGame(Game game) {
-
-		this.game = game;
-	}
-
-	// Return the player of this FavorToken.
-
-	public Player getPlayer() {
-
-		return player;
-	}
-
-	// Set the player of this FavorToken.
-
-	public void setPlayer(Player player) {
-
-		this.player = player;
-	}
-
-	// Return the Toolcard of which this FavorToken is used for.
-
-	public Toolcard getToolcard() {
-
-		return toolcard;
-	}
-
-	// Set the Toolcard of which this FavorToken is used for.
-
-	public void setToolcard(Toolcard toolcard) {
-
-		this.toolcard = toolcard;
-	}
-
-}
-=======
 package model;
 
 import database.DataBaseConnection;
@@ -156,6 +72,4 @@ public class FavorToken {
 		db.setFavortokensForToolCard(playerid, toolcard, round, favortokenid);
 
 	}
-
 }
->>>>>>> 9bc128a chat en favortoken klassen gekoppeld aan database.
