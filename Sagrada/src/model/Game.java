76ebeae12f45a4afa06eb2bfdce90b7f8daeea21
Milanvanoeveren <m.vanoeveren1@student.avans.nl>
2020-06-10 14:22:39 +0200

    changesequencenumber toegevoegd aan setnextplayer

--- Sagrada/src/model/Game.java
+++ Sagrada/src/model/Game.java
@@ -339,6 +339,7 @@ public class Game {
 			System.out.println("still your turn");
 	}
 	public void setNextPlayer() {
+		if(!(getRoundFromDB() >= 20 && getCurrentPlayer().getSequenceNumber() == 1)) {
 			boolean isClockwise = gameDBA.isRoundClockwise(this);
 			
 			if(currentPlayer.getSequenceNumber() == players.size() && isClockwise) {
@@ -388,7 +389,12 @@ public class Game {
 				}
 			} 
 			System.out.println("next player : " + currentPlayer.getId());
+		}
+		else {
+			changeSequenceNumber();
+		}
 	}
+	
 	public void addLeftOverDiceToRoundTrack() {
 		for (int i = 0; i < offer.size(); i++) {
 			gamedieDBA.addDieToRoundTrack(offer.get(i), this, round.get()/2);//TODO check if right
@@ -411,11 +417,12 @@ public class Game {
 					players.get(i).setSequenceNumber(players.get(i).getSequenceNumber()+1);
 				}
 			}
-			if(round.get() >= 20) {
+			if(getRoundFromDB() >= 20 && this.getCurrentPlayer().getSequenceNumber() == 1) {
 				System.out.println("Hier wordt de game beëindigd");
 				finishedGame = true;
-				
-				
+				for (Player p : players) {
+					p.setPlayerStatus(PlayerStatus.FINISHED);
+				}
 			}
 			for (int i = 0; i < players.size(); i++) {
 				if(players.get(i).getSequenceNumber() == 1) {
